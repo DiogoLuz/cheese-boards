f11c6dc9b8b14e7a880bfc0bc917b9735070ba01
DiogoLuz <diogolinoluz@hollandandbarrett.com>
2022-11-04 12:30:29 +0000

    Added needed sequelize property

--- models/board.model.js
+++ models/board.model.js
@@ -4,17 +4,20 @@ const db = require("../db/db");
 
 class Board extends Model {}
 
-Board.init({
-  type: {
-    type: DataTypes.TEXT,
-    allowNull: false,
+Board.init(
+  {
+    type: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+    },
   },
-  description: {
-    type: DataTypes.TEXT,
-  },
-  rating: {
-    type: DataTypes.FLOAT,
-  },
-});
+  { sequelize: db }
+);
 
 module.exports = Board;
