f56eb690d82ad39643958bc1e0be01e1dd6effd0
DiogoLuz <diogolinoluz@hollandandbarrett.com>
2022-11-04 11:34:10 +0000

    Created Board Model

---
+++ models/board.model.js
@@ -0,0 +1,20 @@
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+const db = require("../db/db");
+
+class Board extends Model {}
+
+Board.init({
+  type: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  rating: {
+    type: DataTypes.FLOAT,
+  },
+});
+
+module.exports = Board;
